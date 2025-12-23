b0cd8dfba88608c683ea4f09e481d31c79131970
Tayyab Abdul Hannan <tayyab.abdulhannan@gmail.com>
2025-12-23 14:44:51 +0500

    few more examples were added

---
+++ new.py
@@ -0,0 +1,84 @@
+# # print("tayyab abdul hannan")
+# # print("Hello, World!")
+# # print("Welcome to Python programming.")
+# # print("This is a sample Python script.")
+# # print("Enjoy coding!")
+# # print("Have a great day!")
+# # print("Let's learn Python together.")
+# # print("Keep practicing to improve your skills.")
+# print(3+2)
+import pandas
+# print(pandas.__version__)
+# print("tayyab")
+import hashlib
+# print("\'tayyab \n \"abdul hannan\"")
+# print ("good boy" , 4 , 5 ,7, sep="~" , end="$$")
+# print("hello" , end=" #")
+# print("world")
+# a=12
+# b="tayyab"
+# print(a,b)
+# print(type(a))
+# print(type(b))
+# list = [1,2,3,4.5,5,"tayyab",True, None, [1,2,3], (4,5,6), {7,8,9}]
+# print(list)
+# print(15+10)
+# print(15-10)
+# print(15*10.5)
+# print(15/10)
+# print(15//10)
+# print(15%10)
+# print(15**2)
+# a=5
+# b=2
+# print("the value of a=",a , "the value of b=",b , "the sum of a and b is=", a+b)
+# print("the value of a=",a , "the value of b=",b , "the subtraction of a and b is=", a-b)
+# print("the value of a=",a , "the value of b=",b , "the multiplication of a and b is=", a*b)
+# print("the value of a=",a , "the value of b=",b , "the division of a and b is=", a/b)
+# print("the value of a=",a , "the value of b=",b , "the floor division of a and b is=", a//b)
+# print("the value of a=",a , "the value of b=",b , "the modulo of a and b is=", a%b)
+# print(a%b)
+# a="2"
+# b="3"
+# print(a+b)
+# print(int(a)+int(b))
+# print(str(2)+str(3))
+# a=5
+# b=2.5
+# # print(a+b)
+# print(a+b)
+# x=input()
+# print("my name is",x)
+# print(type(x))
+# a= input("Enter first number: ")
+# b= input("Enter second number: ")
+# print(a+b) 
+# print("the sum is: ", int(a)+int(b))
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+x = "awesome"
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
+
